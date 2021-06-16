a87615a6af77ce8b744300bf6c3a09213dd7f253
nagendrayc504 <nagendra.yc@prixgen.com>
2021-06-16 23:29:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      welcome to reactjs CICD part-8
+    </div>
+  );
+}
+
+export default App;
