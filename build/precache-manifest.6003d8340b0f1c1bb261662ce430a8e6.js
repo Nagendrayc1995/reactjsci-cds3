b34b4df4c7cf86c4375e790da1a191df44b8d782
nagendrayc504 <nagendra.yc@prixgen.com>
2021-06-17 00:43:58 +0530

    2

---
+++ build/precache-manifest.6003d8340b0f1c1bb261662ce430a8e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4db25dd399033571e53e494b814cc8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17a5b44cebff8cc90ff8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7213b92a3fd91183def3",
+    "url": "/static/js/2.3bbe5466.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.3bbe5466.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17a5b44cebff8cc90ff8",
+    "url": "/static/js/main.1ebbd577.chunk.js"
+  },
+  {
+    "revision": "cf6c25936ab84900606e",
+    "url": "/static/js/runtime-main.f56f23b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d1b86ec6dce3c24b98f36b87faffefa6.js"
+  "/precache-manifest.6003d8340b0f1c1bb261662ce430a8e6.js"
 );
 
 self.addEventListener('message', (event) => {
